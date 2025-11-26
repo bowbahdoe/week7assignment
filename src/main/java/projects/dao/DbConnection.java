adf0fa1bcba7833cd05e1fdc18b666aef194506c
Ethan McCue <ethan@mccue.dev>
2025-11-26 11:18:49 -0500

    Format all the code

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -7,26 +7,26 @@ import java.sql.SQLException;
 import projects.exception.DbException;
 
 public class DbConnection {
-	
-	private static String HOST = "localhost";
-	private static String PASSWORD = "projects";
-	private static int PORT = 3306;
-	private static String SCHEMA = "projects";
-	private static String USER = "projects";
-	
-	public static Connection getConnection() {
-		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", 
-				HOST, PORT, SCHEMA, USER, PASSWORD);
-				
-		System.out.println("Connecting uri" + uri);
-		
-		try {
-			Connection conn = DriverManager.getConnection(uri);
-			System.out.println("Connection successful");
-			return conn;
-		} catch (SQLException e) {
-			throw new DbException(e);
-		}
-	}
+
+    private static String HOST = "localhost";
+    private static String PASSWORD = "projects";
+    private static int PORT = 3306;
+    private static String SCHEMA = "projects";
+    private static String USER = "projects";
+
+    public static Connection getConnection() {
+        String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",
+                HOST, PORT, SCHEMA, USER, PASSWORD);
+
+        System.out.println("Connecting uri" + uri);
+
+        try {
+            Connection conn = DriverManager.getConnection(uri);
+            System.out.println("Connection successful");
+            return conn;
+        } catch (SQLException e) {
+            throw new DbException(e);
+        }
+    }
 
 }
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -9,33 +9,34 @@ import projects.entity.Project;
 
 public class ProjectService {
 
-	public Project addProject(Project project) {
-		return projectDao.insertProject(project);
-	}
-	
-	/*
-	 * This method calls the project DAO to retrieve all project rows without accompanying details
-	 * (materials, steps and categories).
-	 * 
-	 *  @return A list of project records.
-	 */
-	private ProjectDao projectDao = new ProjectDao();
-	public List<Project> fetchAllProjects() {
-		return projectDao.fetchAllProjects();
-		
-	}
-	
-	/**
-	 * This method calls the project DAO to get all project details, including materials, steps, and categories.
-	 * If the project ID is invalid, it throws an exception.
-	 * 
-	 * @param projectId The project ID.
-	 * @return A Project object if successful.
-	 *  @throws NoSuchElementException Thrown if the project with the given ID does not exist.
-	 */
-	public Project fetchProjectById(Integer projectId) {
-		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
-				"Project with project ID=" + projectId + " does not exist."));
-	}
+    public Project addProject(Project project) {
+        return projectDao.insertProject(project);
+    }
+
+    /*
+     * This method calls the project DAO to retrieve all project rows without accompanying details
+     * (materials, steps and categories).
+     *
+     *  @return A list of project records.
+     */
+    private ProjectDao projectDao = new ProjectDao();
+
+    public List<Project> fetchAllProjects() {
+        return projectDao.fetchAllProjects();
+
+    }
+
+    /**
+     * This method calls the project DAO to get all project details, including materials, steps, and categories.
+     * If the project ID is invalid, it throws an exception.
+     *
+     * @param projectId The project ID.
+     * @return A Project object if successful.
+     * @throws NoSuchElementException Thrown if the project with the given ID does not exist.
+     */
+    public Project fetchProjectById(Integer projectId) {
+        return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
+                "Project with project ID=" + projectId + " does not exist."));
+    }
 
 }
